8d47b5d7976faee4e960c4363b5fe0adf4a5a740
JFKongphop <kongphopleo@gmail.com>
2024-11-19 23:05:21 +0700

    update call.js

--- shortcuts/call.js
+++ shortcuts/call.js
@@ -6,7 +6,7 @@ const contractName = '';
 const data = require(`../artifacts/contracts/${contractName}.sol/${contractName}.json`);
 
 const providerURL = process.env.CHAIN;
-const privateKey = process.env.PRIVATE_KEY2;
+const privateKey = process.env.PRIVATE_KEY;
 const contractABI = data.abi;
 
 const contractAddress = '';
@@ -15,8 +15,6 @@ const execution = async () => {
   const provider = new ethers.JsonRpcProvider(providerURL);
   const wallet = new ethers.Wallet(privateKey, provider);
   const contract = new ethers.Contract(contractAddress, contractABI, wallet);
-
-
 };
 
 execution().catch((error) => {
